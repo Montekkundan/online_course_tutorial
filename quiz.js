092a3f474e13f751d71c487d76f02efeb699949d
Montek <87750128+Montekkundan@users.noreply.github.com>
2023-03-30 10:12:35 -0700

    added comments, youtube video, fixed bugs

--- quiz.js
+++ quiz.js
@@ -1,54 +1,61 @@
 // JavaScript code to add interactivity to the website
 
-              // Get a reference to the form element
-              var form = document.getElementById("quiz");
+// Get a reference to the form element
+var form = document.getElementById("quiz");
 
-              // Get a reference to the submit button
-              var submit = document.getElementById("submit");
+// Get a reference to the submit button
+var submit = document.getElementById("submit");
 
-              // Get a reference to the feedback element
-              var feedback = document.getElementById("feedback");
+// Get a reference to the feedback element
+var feedback = document.getElementById("feedback");
 
-              // Add an event listener to handle form submission
-              submit.addEventListener("click", function(event) {
+// Add an event listener to handle form submission
+submit.addEventListener("click", function(event) {
 
-                // Prevent default form submission behavior
-                event.preventDefault();
+  // Prevent default form submission behavior
+  event.preventDefault();
 
-                // Declare a variable to store quiz score
-                var score = 0;
+  // Declare a variable to store quiz score
+  var score = 0;
 
-                // Declare an array of correct answers
-                var answers = ["A", "B", "A", "B", "C"];
+  // Declare an array of correct answers
+  var answers = ["A", "B", "A", "B", "C"];
 
-                // Loop through each question in the form
-                for (var i = 0; i < form.elements.length -1 ; i++) {
+  // Loop through each question in the form
+  for (var i = 0; i < answers.length; i++) {
+    // Get the name of the radio buttons for each question
+    var name = "q" + (i + 1);
 
-                  // Check if the input is checked and matches with correct answer
-                  if (form.elements[i].checked && form.elements[i].value == answers[i]) {
+    // Use querySelector to get the checked radio button for each question
+    var checked = form.querySelector("input[name=" + name + "]:checked");
 
-                    // Increment score by one
-                    score++;
+    // Check if the checked radio button matches with correct answer
+    if (checked && checked.value == answers[i]) {
 
-                    // Display correct feedback message
-                    feedback.innerHTML += "<p class='correct'>Question " + (i+1) + ": Correct!</p>";
+      // Increment score by one
+      score++;
 
-                  } else if (form.elements[i].checked && form.elements[i].value != answers[i]) {
+      // Display correct feedback message
+      feedback.innerHTML += "<p class='correct'>Question " + (i+1) + ": Correct!</p>";
 
-                    // Display wrong feedback message with correct answer
-                    feedback.innerHTML += "<p class='wrong'>Question " + (i+1) + ": Wrong! The correct answer is " + answers[i] + "</p>";
+    } else {
 
-                  }
-                }
+      // Display wrong feedback message with correct answer
+      feedback.innerHTML += "<p class='wrong'>Question " + (i+1) + ": Wrong! The correct answer is " + answers[i] + "</p>";
 
-                // Calculate score percentage and round it to two decimal places
-                var percentage = Math.round((score / answers.length) *10000)/100;
+    }
+  }
 
-                // Display score percentage in feedback element
-                feedback.innerHTML += "<p class='score'>Your score is " + percentage + "%</p>";
+  // Calculate score percentage and round it to two decimal places
+  var percentage = Math.round((score / answers.length) *10000)/100;
 
-                // Show feedback element and hide submit button
-                feedback.style.display = "block";
-                submit.style.display = "none";
+  // Display score percentage in feedback element
+  feedback.innerHTML += "<p class='score'>Your score is " + percentage + "%</p>";
+  feedback.innerHTML += '<input class="retry" type="button" value="Reload Page" onclick="window.location.reload()" />';
 
-              });
+
+  // Show feedback element and hide submit button
+  feedback.style.display = "block";
+  submit.style.display = "none";
+
+});
